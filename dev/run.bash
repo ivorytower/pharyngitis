a7504321df13453e0b0018c7d21d5f256c6ed652
Yassen Bantchev <yassenb@gmail.com>
2015-11-04 21:26:51 +0200

    Don't rely on globally installed node modules

--- dev/run.bash
+++ dev/run.bash
@@ -5,4 +5,4 @@ if [ $1 ]; then
     dir=$(readlink -m $1)
 fi
 
-nw $(dirname "${BASH_SOURCE[0]}")/../pharyngitis.nw $dir &
+$(dirname "${BASH_SOURCE[0]}")/../node_modules/.bin/nw $(dirname "${BASH_SOURCE[0]}")/../pharyngitis.nw $dir &
